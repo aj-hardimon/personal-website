205baf9641318260d503bd149e228f344226264a
aj-hardimon <a.hardimon756@gmail.com>
2025-04-08 18:29:16 -0400

    added mongodb implementation to track chatbot responses

--- workspace/backend/index.js
+++ workspace/backend/index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import{GoogleGenerativeAI} from '@google/generative-ai'
+import { MongoClient } from 'mongodb'
 
 dotenv.config()
 
@@ -11,6 +12,14 @@ app.use(cors())
 app.use(bodyParser.json())
 
 const PORT = process.env.PORT || 4000
+
+const mongourl = process.env.MONGO_URL
+const mongoclient = new MongoClient(mongourl, {})
+
+mongoclient.connect().then(() => {
+    console.log("connected to MongoDB")
+})
+
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY)
 const model = genAI.getGenerativeModel({
@@ -29,15 +38,14 @@ const model = genAI.getGenerativeModel({
 `,
 })
 
-app.post(`/chat`, async(req,res) => {
+app.post('/chat', async (req, res) => {
     const userInput = req.body.userInput
     let responseMessage
-    try{
+    try {
         const result = await model.generateContent(userInput)
         responseMessage = result.response.text()
     } catch(e) {
-        console.error(e)
-        responseMessage = "Oops, something went wrong!"
+        responseMessage = 'Oops, something went wrong!'
     }
     res.json({
         message: responseMessage,
@@ -46,4 +54,28 @@ app.post(`/chat`, async(req,res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
+})
+
+app.get('/logs', async (req, res) => {
+    try {
+        const logs = await mongoclient.db('jdt-website').collection('logs').find({}).toArray()
+        res.status(200).json(logs)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Error' })
+    }
+})
+
+app.post('/add', async (req, res) => {
+    try {
+        const log = req.body
+        if (!log.input || !log.response || Object.keys(log).length !== 2) {
+            res.status(400).json({ message: 'Bad Request' })
+            return
+        }
+        await mongoclient.db('jdt-website').collection('logs').insertOne(log)
+    } catch(error) {
+        console.error(error)
+        res.status(500).json({message: 'Error'})
+    }
 })
--- workspace/frontend/src/components/chat/chat.jsx
+++ workspace/frontend/src/components/chat/chat.jsx
@@ -1,23 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './chat.css'
 
 function Chat() {
-    const[messages, setMessages] = useState([])
+    const [messages, setMessages] = useState([])
     const [userInput, setUserInput] = useState('')
+
     async function getResponse() {
-        try{
-            if(!userInput) return
+        try {
+            if (!userInput) return
             const response = await fetch('http://localhost:4000/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({userInput})
+                body: JSON.stringify({ userInput })
             })
             if (!response.ok) {
                 throw new Error('Oops, something went wrong!')
             }
             const { message } = await response.json()
+            fetch('http://localhost:4000/add', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ input: userInput, response: message })
+            })
             setMessages([...messages, userInput, message])
         } catch (error) {
             console.error(error)
@@ -25,22 +33,36 @@ function Chat() {
         }
     }
 
+    useEffect(() => {
+        fetch('http://localhost:4000/logs')
+            .then(res => res.json())
+            .then(data => {
+                let newMessages = []
+                for (let i = 0; i < data.length; i++) {
+                    newMessages.push(data[i].input)
+                    newMessages.push(data[i].response)
+                }
+                setMessages(newMessages)
+            })
+    }, [])
+
     return (
         <div id="chat">
             <form onSubmit={(e) => e.preventDefault()}>
-                <h2>Ask me a question</h2>
-                <input  type='text' 
-                        name='user-input' 
-                        id='questionInput' 
-                        placeholder='What would you like to ask?'
-                        onChange={e => setUserInput(e.target.value)}
-                    />
-                    <button type='submit' onClick={getResponse}>Submit</button>
+                <h2>Ask Me A Question</h2>
+                <input 
+                    type='text' 
+                    name='user-input' 
+                    id='questionInput' 
+                    placeholder='What would you like to ask?' 
+                    onChange={e => setUserInput(e.target.value)}
+                />
+                <button type='submit' onClick={getResponse}>Submit</button>
             </form>
             {
                 messages.map((text, index) => (
                     <div key={index} className='chatbox'>
-                        <p className={index % 2 == 0 ? 'user-message' : 'chatbotResponse'}>{text}</p>
+                        <p className={index % 2 == 0 ? 'user-message' : 'chatbot-response'}>{text}</p>
                     </div>
                 ))
             }
